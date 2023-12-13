f389e2f7e3c4421bd65d5bc24ee368b0433d8155
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2023-12-13 23:55:21 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
+./cloudflared update
 ./xray run -c ./xray.config.json &
-ls ~/.cloudflared
-./cloudflared tunnel --url http://127.0.0.1:50309/
 ./cloudflared tunnel run --url http://127.0.0.1:50309/ v2ray_1
